276f1a7c40a347d0f6948fd3fbb63dbe0e14eba8
sfwservers <80946943+sfwservers@users.noreply.github.com>
2024-01-24 19:11:51 +0800

    Create autorestart.sh

---
+++ autorestart.sh
@@ -0,0 +1,15 @@
+ 0.30 KB | None |  
+     
+#!/bin/bash
+ 
+LOGFILE="./crash.log" 
+ 
+while true; do
+  ./PalServer.sh
+  EXIT_STATUS=$? # Capture the exit status
+ 
+  # Log the date, time, and exit status to the logfile
+  echo "Server crashed with exit status $EXIT_STATUS at $(date)" >> "$LOGFILE"
+ 
+  sleep 60 # Wait for 1min before restarting
+done
